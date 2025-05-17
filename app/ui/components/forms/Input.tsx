bbe2cc4bda681d27fe830dff4747dea418c6f5c2
WP-Anh <anhhainb198@gmail.com>
2025-05-17 22:42:02 +0700

    edit all

--- app/ui/components/forms/Input.tsx
+++ app/ui/components/forms/Input.tsx
@@ -16,11 +16,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           </label>
         )}
         <input
-          className={twMerge(
-            "w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all duration-200",
-            error && "border-red-500 focus:ring-red-500",
-            className
-          )}
+          className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all duration-200"
           ref={ref}
           {...props}
         />
